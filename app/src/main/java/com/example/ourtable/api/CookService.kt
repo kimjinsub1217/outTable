b0da0485bd0b447ba96963b9c9c70ee0db18cf29
kimjinsub <hikm9985@naver.com>
2022-05-31 00:44:18 +0900

    README.md 수정

--- app/src/main/java/com/example/ourtable/api/CookService.kt
+++ app/src/main/java/com/example/ourtable/api/CookService.kt
@@ -1,5 +1,7 @@
 package com.example.ourtable.api
 
+import android.os.Build
+import com.example.ourtable.BuildConfig
 import com.example.ourtable.model.Cook
 import retrofit2.Call
 import retrofit2.http.GET
@@ -7,7 +9,7 @@ import retrofit2.http.GET
 interface CookService {
 
 
-    @GET("/api/d6b4f5552c2b4b549fac/COOKRCP01/json/1/5")
+    @GET("/api/${BuildConfig.ourTable}/COOKRCP01/json/1/5")
     fun getMoviesByName(): Call<Cook>
 
 }
